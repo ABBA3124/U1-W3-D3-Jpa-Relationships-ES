9a79dd0b8982209c50205ec00c1e3418d61bfaea
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-05 14:56:59 +0200

    add all entities
    Evento, Location, Partecipazione, Persona

---
+++ src/main/java/org/davideabbadessa/Application.java
@@ -0,0 +1,9 @@
+package org.davideabbadessa;
+
+public class Application {
+//    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("U1-W3-D3-Jpa-Relationships");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- src/test/java/org/davideabbadessa/ApplicationTest.java
+++ src/test/java/org/davideabbadessa/ApplicationTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.davideabbadessa;
 
 import org.junit.jupiter.api.Test;
 
