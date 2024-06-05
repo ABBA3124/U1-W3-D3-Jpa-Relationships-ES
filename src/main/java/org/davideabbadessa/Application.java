d79e5da050d3bea9a9097a1d9ee00523468f2747
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-05 15:47:17 +0200

    Creo istanze DAO per le operazioni di persistenza
    Creo istanze DAO per le operazioni di persistenza

--- src/main/java/org/davideabbadessa/Application.java
+++ src/main/java/org/davideabbadessa/Application.java
@@ -2,6 +2,10 @@ package org.davideabbadessa;
 
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import org.davideabbadessa.dao.EventoDAO;
+import org.davideabbadessa.dao.LocationDAO;
+import org.davideabbadessa.dao.PartecipazioneDAO;
+import org.davideabbadessa.dao.PersonaDAO;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("U1-W3-D3-Jpa-Relationships");
@@ -10,6 +14,11 @@ public class Application {
 
         System.out.println("Start U1-W3-D3-Jpa-Relationships-ES");
 
+        PersonaDAO personaDAO = new PersonaDAO();
+        LocationDAO locationDAO = new LocationDAO();
+        EventoDAO eventoDAO = new EventoDAO();
+        PartecipazioneDAO partecipazioneDAO = new PartecipazioneDAO();
 
+        
     }
 }
