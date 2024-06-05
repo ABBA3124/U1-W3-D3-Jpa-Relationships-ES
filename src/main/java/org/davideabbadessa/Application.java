7da097fa2448c7bb35a5f93aac9e9e76c23b38f2
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-05 16:34:50 +0200

    FINISH

--- src/main/java/org/davideabbadessa/Application.java
+++ src/main/java/org/davideabbadessa/Application.java
@@ -1,11 +1,14 @@
 package org.davideabbadessa;
 
+import davideabbadessa.entities.Evento;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.davideabbadessa.dao.EventoDAO;
 import org.davideabbadessa.dao.LocationDAO;
 import org.davideabbadessa.dao.PartecipazioneDAO;
 import org.davideabbadessa.dao.PersonaDAO;
+import org.davideabbadessa.entities.Location;
+import org.davideabbadessa.entities.Partecipazione;
 import org.davideabbadessa.entities.Persona;
 
 import java.time.LocalDate;
@@ -25,9 +28,42 @@ public class Application {
         EventoDAO eventoDAO = new EventoDAO();
         PartecipazioneDAO partecipazioneDAO = new PartecipazioneDAO();
 
-        Persona persona = new Persona("Davide", "Abbadessa", "davideabbadessa@gmail.com", LocalDate.of(1999, 3, 2), 'M', new ArrayList<>());
+
+        //CREO ENTITà  E LE SALVO
+
+        //PERSONA
+        Persona persona = new Persona("Alessio", "Vulpinari", "alessiovulpinari@gmail.com", LocalDate.of(1999, 7, 3), 'F', new ArrayList<>());
 
         personaDAO.save(persona);
-        System.out.println("" + persona.getNome() + " " + persona.getCognome() + " Aggiunta con successo");
+        System.out.println("User: " + persona.getNome() + " " + persona.getCognome() + " Aggiunto/a con successo");
+
+        //LOCATION
+        Location location = new Location("Sagra della salsiccia", "Rimini");
+
+        locationDAO.save(location);
+        System.out.println(" " + persona.getNome() + " " + persona.getCognome() + " Aggiunto/a con successo all'evento: " + location.getNome() + " Che si terrà: " + location.getCitta());
+
+        //EVENTO
+        davideabbadessa.entities.Evento evento = new Evento(new ArrayList<>(), location, 53241, Evento.EventType.PUBBLICO, "Carnevale", LocalDate.of(2024, 10, 12), "Salsiccia a volontà");
+
+        eventoDAO.save(evento);
+        System.out.println("Evento creato con successo !");
+
+        //CREO LA PARTECIPAZIONE E LA SALVO
+        Partecipazione partecipazione = new Partecipazione(persona, evento, Partecipazione.Stato.DA_CONFERMARE);
+
+        partecipazioneDAO.save(partecipazione);
+
+        //REFRESH LISTE PARTECIPAZIONE
+        persona.getListaPartecipazioni().add(partecipazione);
+        evento.getListaPartecipazioni().add(partecipazione);
+
+        //AGGIORNO ENTITà NEL DB
+        personaDAO.update(persona);
+        eventoDAO.update(evento);
+
+        System.out.println("User: " + persona.getNome() + " " + persona.getCognome() + " " + "Location: " + "Evento: " + evento.getTitle() + " Partecipazione sono stati salvati con successo dentro il DB. ");
+
+
     }
 }
